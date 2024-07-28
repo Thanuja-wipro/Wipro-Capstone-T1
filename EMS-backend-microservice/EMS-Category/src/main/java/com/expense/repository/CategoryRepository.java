27a64e9bcbd04b1e321fb67e2b9b1391f7502d0a
AvinashPalacharla <avinashpalacharla05@gmail.com>
2024-07-28 13:39:40 +0530

    removed service files

--- EMS-backend-microservice/EMS-Category/src/main/java/com/expense/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expense.repository;
-
-import com.expense.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-
-}
--- EMS-backend-microservice/EMS-Report/src/main/java/com/expense/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.expense.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.expense.entity.Report;
-
-public interface ReportRepository  extends JpaRepository<Report, Long>{
-
-}
