b21df6a177254771d043007a560bebe368959879
Vamsi Krishna Ambati <vamsikrishna66631@gmail.com>
2024-07-30 23:06:01 +0530

    removed old microservices

--- EMS-backend-microservice/EMS-Expense/src/main/java/com/expense/dto/UserDto.java
+++
@@ -1,5 +0,0 @@
-package com.expense.dto;
-
-public class UserDto {
-
-}
--- EMS-backend-microservice/EMS-Notification/src/main/java/com/expense/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.expense.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.expense.entity.Notification;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
-}
